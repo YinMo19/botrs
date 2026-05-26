90f98b924fc97d29a885ce7a109304646f9761b1
YinMo19 <me@yinmo19.top>
2026-05-27 04:34:52 +0800

    Add botpy interaction result API

--- src/client/context/interaction.rs
+++ src/client/context/interaction.rs
@@ -2,6 +2,12 @@ use super::Context;
 use crate::client::prelude::*;
 
 impl Context {
+    pub async fn on_interaction_result(&self, interaction_id: &str, code: i32) -> Result<()> {
+        self.api
+            .on_interaction_result(&self.token, interaction_id, code)
+            .await
+    }
+
     pub async fn put_interaction(&self, interaction_id: &str, body: &str) -> Result<()> {
         self.api
             .put_interaction(&self.token, interaction_id, body)
