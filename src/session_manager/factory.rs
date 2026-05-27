18f4c65a55800e5b15389c51c61568018d317380
YinMo19 <me@yinmo19.top>
2026-05-28 01:40:17 +0800

    Remove redundant session manager setter

--- src/session_manager/factory.rs
+++ src/session_manager/factory.rs
@@ -1,6 +1,6 @@
 use std::sync::{LazyLock, RwLock};
 
-use super::{BoxedSessionManager, ChanManager, SessionManager};
+use super::{BoxedSessionManager, ChanManager};
 
 pub type SessionManagerFactory = dyn Fn() -> BoxedSessionManager + Send + Sync;
 
@@ -20,8 +20,4 @@ pub fn set_session_manager_factory(
     *DEFAULT_SESSION_MANAGER
         .write()
         .expect("default session manager lock poisoned") = Box::new(factory);
-}
-
-pub fn set_session_manager(manager: impl SessionManager + Clone + 'static) {
-    set_session_manager_factory(move || Box::new(manager.clone()));
 }
--- src/session_manager/mod.rs
+++ src/session_manager/mod.rs
@@ -11,9 +11,7 @@ pub use channel::ChanManager;
 pub use errors::{
     CANNOT_IDENTIFY_ERROR_CODES, CANNOT_RESUME_ERROR_CODES, can_not_identify, can_not_resume,
 };
-pub use factory::{
-    SessionManagerFactory, new_session_manager, set_session_manager, set_session_manager_factory,
-};
+pub use factory::{SessionManagerFactory, new_session_manager, set_session_manager_factory};
 pub use limit::{calc_interval, check_session_limit};
 pub use session::Session;
 pub use traits::{BoxedSessionManager, SessionConnectFn, SessionFuture, SessionManager};
